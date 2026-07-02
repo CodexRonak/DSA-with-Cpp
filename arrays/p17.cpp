a6a605ef686d289a76f4283747ae80906d36ed85
Ronak <ronakrp2004@gmail.com>
2026-07-02 23:24:08 +0530

    sqrt using binary search

---
+++ arrays/p17.cpp
@@ -0,0 +1,55 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+// Constraints:
+
+// 0 <= x <= 231 - 1
+
+
+#include<iostream>
+using namespace std;
+
+long long int sqroot(int num){
+    int s = 0, e = num;
+    long long int ans = 0;
+    int m = s + (e - s) / 2;
+    if (num == 0 || num == 1) return num;
+    while (s <= e){
+        long long sq = m * m;
+        if (sq == num ){
+            ans = m;
+            return ans;
+        }
+        else if (sq > num){
+            e = m - 1;
+        }
+        else if (m * m < num){
+            ans = m;
+            s = m + 1;
+        }
+        m = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int main(){
+    int num = 10000;
+    cout << sqroot(num) << endl;
+    return 0;
+}

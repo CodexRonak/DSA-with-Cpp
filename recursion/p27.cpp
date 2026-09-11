b2acb7dcf4116e4054489e132294c239b6e8e52a
Ronak <ronakrp2004@gmail.com>
2026-09-11 23:21:20 +0530

    permutation using recursion

---
+++ recursion/p27.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void permutation(string str, int index, vector<string> &ans){
+    if(index >= str.length()){
+        ans.push_back(str);
+        return;
+    }
+
+    for (int i = index; i < str.length(); i++){
+        swap(str[index], str[i]);
+        permutation(str, index + 1, ans);
+        // backtracking
+        swap(str[index], str[i]);
+    }
+}
+
+int main(){
+    string str = "abc";
+    int index = 0;
+    vector<string> ans;
+
+    permutation(str, index, ans);
+
+    for (int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
